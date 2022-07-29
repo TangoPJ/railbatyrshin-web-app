ab12c4dd7aab822dab231f8d7d93ac46001da629
Rail Batyrshin <rail.batyrshin.js@gmail.com>
2022-07-29 19:25:54 +0600

    feat: refactor the code and styles, divide header to another component

---
+++ components/layouts/header/index.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+import myPhoto from '../../../assets/images/my-photo.avif';
+import styles from './Header.module.scss';
+
+const Header: React.FC = () => {
+  return (
+    <header className={styles.header}>
+      <div className="container animate__animated animate__fadeIn">
+        <div className={styles.header__image}>
+          <Image
+            src={myPhoto}
+            alt="Rail Batyrshin"
+            width={100}
+            height={100}
+            layout="responsive"
+          />
+        </div>
+        <h1 className={styles.header__title}>Rail Batyrshin</h1>
+        <h2 className={styles.header__subtitle}>
+          Hello & Welcome To My Site, I&apos;m a JavaScript developer with over a year experience in web development at <a href="https://maddevs.io" className={styles["header__company-link"]} target="_blank" rel="noreferrer">MadDevs</a>. My primary areas of focus and expertise are JavaScript, TypeScript, React, and Next.js.
+        </h2>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
 import Head from 'next/head';
-import Image from 'next/image';
-import myPhoto from '../assets/images/my-photo.avif';
+import Header from '../components/layouts/header';
 import PrimaryLayout from '../components/layouts/primary';
-import styles from '../styles/Home.module.scss';
 import { NextPageWithLayout } from './page';
 
 const Home: NextPageWithLayout = () => {
@@ -11,20 +9,7 @@ const Home: NextPageWithLayout = () => {
       <Head>
         <title>Rail Batyrshin: Home</title>
       </Head>
-      <header className={styles.header}>
-        <div className={styles.header__content}>
-          <div className={styles.header__image}>
-            <Image
-              src={myPhoto}
-              alt="Rail Batyrshin"
-              width={100}
-              height={100}
-              layout="responsive"
-            />
-          </div>
-          <h1 className={styles.header__title}>Rail Batyrshin</h1>
-        </div>
-      </header>
+      <Header />
     </>
   );
 };
