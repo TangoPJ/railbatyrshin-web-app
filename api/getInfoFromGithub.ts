ffc8d63d676ee7dbb3e6a835a77c05a0506c65ae
Rail Batyrshin <rail.batyrshin.js@gmail.com>
2022-08-23 23:39:00 +0600

    fix: run prettier

--- api/getInfoFromGithub.ts
+++ api/getInfoFromGithub.ts
@@ -1,9 +1,9 @@
-import { Octokit } from "@octokit/core";
+import { Octokit } from '@octokit/core';
 
 export type USER = {
   user: string;
   repository: string;
-}
+};
 
 export type GITHUB_INFO = {
   name: string;
@@ -11,33 +11,35 @@ export type GITHUB_INFO = {
   stars: number;
   forks: number;
   url: string;
-}
+};
 
 export const getInfoFromGit = async (urls: Array<USER>): Promise<any> => {
   const octokit = new Octokit({
     auth: process.env.NEXT_PUBLIC_GITHUB_TOKEN,
-  })
+  });
 
-  const repositories = []
+  const repositories = [];
 
   for (const url of urls) {
     try {
-      const { data: { name, description, forks, stargazers_count, html_url } } = await octokit.request(`GET /repos/${url.user}/${url.repository}`, {
+      const {
+        data: { name, description, forks, stargazers_count, html_url },
+      } = await octokit.request(`GET /repos/${url.user}/${url.repository}`, {
         owner: 'OWNER',
-        repo: 'REPO'
-      })
+        repo: 'REPO',
+      });
       repositories.push({
         name,
         description,
         forks,
         stars: stargazers_count,
-        url: html_url
-      })
+        url: html_url,
+      });
     } catch (error) {
       console.log(error);
-      throw new Error(`Error: ${error}`)
+      throw new Error(`Error: ${error}`);
     }
   }
 
-  return repositories
-}
+  return repositories;
+};
--- components/layouts/gitContent/index.tsx
+++ components/layouts/gitContent/index.tsx
@@ -1,49 +1,62 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
-import { getInfoFromGit, GITHUB_INFO, USER } from '../../../api/getInfoFromGithub';
+import {
+  getInfoFromGit,
+  GITHUB_INFO,
+  USER,
+} from '../../../api/getInfoFromGithub';
 import Fork from '../../../assets/images/svg/fork.svg';
 import Star from '../../../assets/images/svg/star.svg';
 import styles from './gitContent.module.scss';
 
 export const GitContent: React.FC = () => {
-  const [repos, setRepos] = useState<Array<GITHUB_INFO>>([])
+  const [repos, setRepos] = useState<Array<GITHUB_INFO>>([]);
 
   useEffect(() => {
     const urls = [
       { user: 'fakng-agrgtr', repository: 'fakng-ui' },
       { user: 'TangoPJ', repository: 'railbatyrshin-web-app' },
       { user: 'maddevsio', repository: 'maddevs' },
-      { user: 'maddevsio', repository: 'mad-radiator' }
-    ]
+      { user: 'maddevsio', repository: 'mad-radiator' },
+    ];
     const fetchData = async (urls: Array<USER>) => {
-      const repositories = await getInfoFromGit(urls)
-      setRepos(repositories)
-    }
-    fetchData(urls)
-  }, [])
+      const repositories = await getInfoFromGit(urls);
+      setRepos(repositories);
+    };
+    fetchData(urls);
+  }, []);
 
   return (
     <section className={styles['git-bg']}>
-      <div className='container'>
-        <h1 className={`${styles['git-bg__title']} animate__animated animate__fadeInUp`}>Repositories I&apos;m working on</h1>
+      <div className="container">
+        <h1
+          className={`${styles['git-bg__title']} animate__animated animate__fadeInUp`}
+        >
+          Repositories I&apos;m working on
+        </h1>
         <div className={styles.cards}>
-          {repos.map(repo => {
+          {repos.map((repo) => {
             return (
-              <div className={`${styles.cards__card} animate__animated animate__fadeInUp`} key={repo.name} >
+              <div
+                className={`${styles.cards__card} animate__animated animate__fadeInUp`}
+                key={repo.name}
+              >
                 <div>
-                  <p className={styles['cards__card-title']}>
-                    {repo.name}
-                  </p>
+                  <p className={styles['cards__card-title']}>{repo.name}</p>
                   <div className={styles['cards__card-subtitle']}>
                     <span className={styles['cards__card-icon']}>
                       <Image src={Star} alt="Github" width={25} height={25} />
                     </span>
-                    <span className={styles['cards__card-text']}>{repo.stars}</span>
+                    <span className={styles['cards__card-text']}>
+                      {repo.stars}
+                    </span>
                     <span className={styles['cards__card-icon']}>
                       <Image src={Fork} alt="Github" width={20} height={20} />
                     </span>
-                    <span className={styles['cards__card-text']}>{repo.forks}</span>
+                    <span className={styles['cards__card-text']}>
+                      {repo.forks}
+                    </span>
                   </div>
                   <p className={styles['cards__card-description']}>
                     {repo.description}
@@ -52,16 +65,20 @@ export const GitContent: React.FC = () => {
                 <div className={styles['cards__card-footer']}>
                   <hr className={styles['cards__card-line']} />
                   <Link href={repo.url}>
-                    <a target="_blank" className={styles['cards__card-footer--link']} rel="noopener noreferrer">
+                    <a
+                      target="_blank"
+                      className={styles['cards__card-footer--link']}
+                      rel="noopener noreferrer"
+                    >
                       VIEW REPO
                     </a>
                   </Link>
                 </div>
               </div>
-            )
+            );
           })}
         </div>
       </div>
-    </section >
+    </section>
   );
 };
